220863447134ae13e6f78a0338caa6d60a1182e7
MD ABDULLAH ALL MAMUN <mamun700263@gmail.com>
2025-11-15 13:27:21 +0000

    feature #3: perfectly working celery

--- app/tasks/__init__.py
+++ app/tasks/__init__.py
@@ -1,8 +1,6 @@
-from .checker import ping
 from .google_map_scraper import run_scraper
 
 
 __all__=[
-    "ping",
     "run_scraper"
 ]
--- app/tasks/checker.py
+++
@@ -1,5 +0,0 @@
-from app.core.celery import celery_app
-
-@celery_app.task(name="demo.ping")
-def ping():
-    return "pong"
